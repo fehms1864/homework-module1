6248c5607fb44bf4ece5499dd8eacb93905ea633
Fehmina <fehmina.hasan@gmail.com>
2024-02-10 00:01:01 -0800

    week 5 day 1 homework: add jquery script to select and change cell color on click

--- js/activitycell.js
+++ js/activitycell.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-    
+    $("tr td").click(function() {
+        //Checks if the element has a location available for the activity and
+        //the element is not a cell with the activity written on it.
+       if( $(this).text() != "Not Available" && $(this).index() != 0 ) {
+            //Change cell color to darker green otherwise go back to original color
+            if( $(this).css("background-color") != "rgb(174, 223, 0)" ) {
+                $(this).css("background-color","rgb(174, 223, 0)");
+                $(this).css('color','white');
+            } else {
+                $(this).css("background-color","");
+                $(this).css('color','');
+            }
+       }
+    });
+    $("tr td").hover(function() {
+        //Checks if the element has a location available for the activity and
+        //the element is not a cell with the activity written on it.
+        //then change cursor to show a pointer
+       if( $(this).text() != "Not Available" && $(this).index() != 0 ) {
+        $(this).css("cursor","pointer");
+       }
+    });
+
 })
